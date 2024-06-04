a51a796013ef789b392a666b58e78f1252a4b4ef
Pavlos Spanidis <p.spanidis@gmail.com>
2024-06-04 12:49:39 +0300

    2nd Login page 1st Enter page

--- app/src/main/java/el/ps/nextetrucknewtemplate/MainActivity.java
+++ app/src/main/java/el/ps/nextetrucknewtemplate/MainActivity.java
@@ -2,13 +2,27 @@ package el.ps.nextetrucknewtemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEnter = findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Page2LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/el/ps/nextetrucknewtemplate/Page2LoginActivity.java
@@ -0,0 +1,14 @@
+package el.ps.nextetrucknewtemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page2LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2_login);
+    }
+}
